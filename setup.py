c179fefef0dda19a62436d712c91f861fcba3842
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2022-04-23 14:27:43 +0100

    setup cfg created

--- setup.py
+++ setup.py
@@ -1,14 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    # metadata
-    name='chembo', 
-    version='1.0', 
-    description="Chembo testing",
-    author="Stefan Pricopie",
-    author_email="stefan.pricopie@postgrad.manchester.ac.uk",
-
-    # options
-    packages=find_packages(),
-    package_dir="src"
-)
+from setuptools import setup
+setup()
